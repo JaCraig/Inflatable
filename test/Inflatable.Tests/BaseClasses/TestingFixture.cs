b2e14c4570a4b310c1fc04b462b285e4738a796d
James Craig <craig.jamesa@gmail.com>
2020-05-09 19:32:06 -0400

    That made it worse.

--- test/Inflatable.Tests/BaseClasses/TestingFixture.cs
+++ test/Inflatable.Tests/BaseClasses/TestingFixture.cs
@@ -7,17 +7,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.ObjectPool;
 using Serilog;
 using SQLHelperDB;
-using System;
-using System.Data;
-using System.Data.SqlClient;
 using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Inflatable.Tests.BaseClasses
 {
     [Collection("Test collection")]
-    public abstract class TestingFixture : IDisposable
+    public abstract class TestingFixture
     {
         public TestingFixture()
         {
@@ -34,19 +30,5 @@ namespace Inflatable.Tests.BaseClasses
         public static MappingManager MappingManager => Canister.Builder.Bootstrapper.Resolve<MappingManager>();
         public static ObjectPool<StringBuilder> ObjectPool => Canister.Builder.Bootstrapper.Resolve<ObjectPool<StringBuilder>>();
         public static Sherlock Sherlock => Canister.Builder.Bootstrapper.Resolve<Sherlock>();
-
-        public void Dispose()
-        {
-            try
-            {
-                Task.Run(async () => await Helper.CreateBatch(SqlClientFactory.Instance, "Data Source=localhost;Initial Catalog=master;Integrated Security=SSPI;Pooling=false")
-                    .AddQuery(CommandType.Text, "ALTER DATABASE TestDatabase SET OFFLINE WITH ROLLBACK IMMEDIATE\r\nALTER DATABASE TestDatabase SET ONLINE\r\nDROP DATABASE TestDatabase")
-                    .AddQuery(CommandType.Text, "ALTER DATABASE TestDatabase2 SET OFFLINE WITH ROLLBACK IMMEDIATE\r\nALTER DATABASE TestDatabase2 SET ONLINE\r\nDROP DATABASE TestDatabase2")
-                    .AddQuery(CommandType.Text, "ALTER DATABASE MockDatabase SET OFFLINE WITH ROLLBACK IMMEDIATE\r\nALTER DATABASE MockDatabase SET ONLINE\r\nDROP DATABASE MockDatabase")
-                    .AddQuery(CommandType.Text, "ALTER DATABASE MockDatabaseForMockMapping SET OFFLINE WITH ROLLBACK IMMEDIATE\r\nALTER DATABASE MockDatabaseForMockMapping SET ONLINE\r\nDROP DATABASE MockDatabaseForMockMapping")
-                    .ExecuteScalarAsync<int>().ConfigureAwait(false)).GetAwaiter().GetResult();
-            }
-            catch { }
-        }
     }
 }
