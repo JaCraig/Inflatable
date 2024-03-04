7aeef3fe4ad848bbaa9283385e0b2c354fb0b00b
JaCraig <craig.jamesa@gmail.com>
2024-03-04 15:48:38 -0500

    fix: Fixing many to one issue
    1. The `BenchmarkDotNet` package in `Inflatable.Benchmarks.csproj` was updated from version `0.13.11` to `0.13.12`. This package is used for benchmarking code performance.
    
    2. The `DragonHoard.InMemory` package in `Inflatable.csproj` was updated from version `2.0.21` to `2.0.27`. This package is used for in-memory caching.
    
    3. The `Holmes` package in `Inflatable.csproj` was updated from version `4.0.115` to `4.0.148`. This package is used for logging and diagnostics.
    
    4. The `Data.Modeler` package in `Inflatable.csproj` was updated from version `4.0.137` to `4.0.147`. This package is used for data modeling.
    
    5. The `Valkyrie` package in `Inflatable.csproj` was updated from version `4.0.51` to `4.0.57`. This package is used for validation.
    
    6. A new class `IManyToOneSingleStartMethod` was added to `IManyToOneSingleStartMethod.cs`. This class implements the `IStartMethodHelper` interface. This adds the many to one fix when setting an object.

---
+++ src/Inflatable/Aspect/StartMethod/IManyToOneSingleStartMethod.cs
@@ -0,0 +1,41 @@
+﻿using Inflatable.Aspect.Interfaces;
+using Inflatable.ClassMapper.Interfaces;
+using Inflatable.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inflatable.Aspect.StartMethod
+{
+    /// <summary>
+    /// Many to one single property start method
+    /// </summary>
+    /// <seealso cref="IStartMethodHelper"/>
+    public class IManyToOneSingleStartMethod : IStartMethodHelper
+    {
+        /// <summary>
+        /// Sets up the specified method.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        /// <param name="mapping">The mapping.</param>
+        /// <param name="builder">The builder.</param>
+        public void Setup(MethodInfo method, IMapping mapping, StringBuilder builder)
+        {
+            if (mapping is null || builder is null)
+                return;
+            var Property = mapping.ManyToOneProperties.Find(x => x.Name == method.Name.Replace("set_", string.Empty, StringComparison.Ordinal));
+            if (Property is null)
+                return;
+
+            if (Property is IManyToOneListProperty)
+                return;
+
+            builder.Append(Property.InternalFieldName).AppendLine(" = value;")
+                .Append(Property.InternalFieldName).AppendLine("Loaded = true;")
+                .Append("NotifyPropertyChanged0(\"").Append(Property.Name).AppendLine("\");");
+        }
+    }
+}
